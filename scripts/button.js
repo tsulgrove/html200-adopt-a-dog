f8a224c6ee378f3c3381dca10d3346cde67914b6
Thomas Sulgrove <tsulgrov@amazon.com>
2023-08-08 15:34:28 -0600

    add in alerts for buttons

---
+++ scripts/button.js
@@ -0,0 +1,11 @@
+let total = 0;
+
+function addDogCost(cost) {
+  total = total + Number(cost);
+  alert(total)
+}
+
+function alertDogInfo (name, breed, fees) {
+  alert(`Name: "${name}", Breed: "${breed}", Fees: $${fees}`);
+  
+}
