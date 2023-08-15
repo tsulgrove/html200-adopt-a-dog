f25b6c9da78a2aedcc4c4df49b25390eea877ae0
Thomas Sulgrove <tsulgrov@amazon.com>
2023-08-14 18:30:48 -0600

    rm old code

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,19 +1,3 @@
-/**
-const form = document.querySelector("form")
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    console.log(document.getElementById("contactName").value);
-    console.log(document.getElementById("contactEmail").value);
-    console.log(document.getElementById("contactAddress").value);
-    console.log(document.getElementById("contactCity").value);
-    console.log(document.getElementById("contactState").value);
-    console.log(document.getElementById("contactZip").value);
-    console.log(document.getElementById("firstTimeYes").checked);
-    console.log(document.getElementById("firstTimeNo").checked);
-    alert("Thank you. The form information has been received");
-});
-*/
 $(function() {
     $('form').submit(function(event) {
         event.preventDefault();
